195d62c0d951e53f5b332eba0c3e36876dc115c2
Lingxi Shang <lingxi.shang@gmail.com>
2026-07-30 23:31:10 -0400

    Update RelayBar cask to 1.3.0

--- Casks/relaybar.rb
+++ Casks/relaybar.rb
@@ -1,6 +1,6 @@
 cask "relaybar" do
-  version "1.2.1"
-  sha256 "71ea4cfd18a703e9dbcd5085c2b6b387dcd82635f9dbd99bae07a11adf51a3b8"
+  version "1.3.0"
+  sha256 "127670e8e5afa51e92ea65c51ca3f56144f85b7f54bda218d517b3dd4f17aa7a"
 
   url "https://github.com/lx2026/RelayBar/releases/download/v#{version}/RelayBar.zip",
       verified: "github.com/lx2026/RelayBar/"
